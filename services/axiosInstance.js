38f56467ef9b2ef8e26c45c1661dbae0048096f1
AndreyNovaes <andreynovaespro@gmail.com>
2023-01-09 12:25:10 -0300

    chore: add constants file with a axiosIstance removed unused file

--- services/axiosInstance.js
+++ services/axiosInstance.js
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from "axios";
+import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
---
+++ utils/constants.js
@@ -0,0 +1,3 @@
+export const JWT_SECRET=process.env.JWT_SECRET ?? 'secret';
+export const SALT_ROUNDS=10;
+export const SECONDS_OF_A_DAY=86400;
